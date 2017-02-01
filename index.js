cc4ae80087802469eaebfd660034e4d5829b57ae
Charlie Duong <charlieduong94@gmail.com>
2017-02-01 15:54:19 -0500

    Fixed bug related to not being able to append notifications to different targets

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var Snackbar = require('./src/components/snackbar');
 var SnackContainer = require('./src/components/snack-container');
+var _createUUID = require('uuid/v4');
 
 var positions = [
     'tr',
@@ -22,9 +23,17 @@ exports.createNotification = function(options, targetEl) {
     // default target is the document's body
     targetEl = targetEl || document.body;
 
+    // retrieve unique id of the element that notification containers are being attached to
+    var targetId;
+    if (targetEl._snackbarContainerId) {
+        targetId = targetEl._snackbarContainerId;
+    } else {
+        targetId = targetEl._snackbarContainerId = _createUUID();
+    }
+
     var position = options.position;
     if (!position) {
-        // default to top left
+        // default to top right
         position = 'tr';
     } else if (positions.indexOf(position) === -1) {
         throw new Error('Invalid position specified. Accepted values: ' + positions.join(', '));
@@ -32,18 +41,29 @@ exports.createNotification = function(options, targetEl) {
 
     var direction = position[0] === 't' ? 'down' : 'up';
 
-    if (!targets[targetEl]) {
-        targets[targetEl] = {};
+    if (!targets[targetId]) {
+        targets[targetId] = {};
     }
+
     // lazily render the container element for notifications when the position is given
-    var containerWidget = targets[targetEl][position];
+    var containerWidget = targets[targetId][position];
     if (!containerWidget) {
         var containerEl = SnackContainer.renderSync({
             position: position,
             direction: direction
         });
+
         containerEl.appendTo(targetEl);
-        containerWidget = targets[targetEl][position] = containerEl.getWidget();
+        containerWidget = targets[targetId][position] = containerEl.getWidget();
+
+        // clean up dead widgets
+        containerWidget.on('destroy', function() {
+            delete targets[targetId][position];
+            if (Object.keys(targets[targetId]).length === 0) {
+                // targetElement does not have any other container elements
+                delete targets[targetId];
+            }
+        });
     }
     options.transitionDirection = direction;
 
